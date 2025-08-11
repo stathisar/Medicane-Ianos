a839b9819ebd67c0534da2beeb655912aa1d2cd4
Stathis G. Arapostathis <31962431+stathisar@users.noreply.github.com>
2025-08-11 19:34:14 +0300

    Update flickr.py
    Flickr scraper with official API keys for getting the thematic data.

--- scraper/flickr.py
+++ scraper/flickr.py
@@ -1,15 +1,34 @@
 import flickrapi
 import csv
 import time
+from typing import Optional
+from datetime import datetime
 
-# Add your API keys
-API_KEY = 'APISTRING'
-API_SECRET = 'APISECRET'
+# Add the corresponding API keys
+API_KEY = 'f0e5e766faf72869d9ec887e882e5b55'
+API_SECRET = '6117f46de7ed60fb'
 
 # Creation of Flickr API client
 flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET, format='parsed-json')
 
-def scrape_flickr(tag, max_pages=5, per_page=100, output_csv='flickr_photos.csv'):
+
+def date_to_unix(date_str: Optional[str]) -> Optional[int]:
+    if date_str:
+        dt = datetime.strptime(date_str, '%Y-%m-%d')
+        return int(dt.timestamp())
+    return None
+    
+    
+def scrape_flickr(
+    tag: str,
+    max_pages: int = 5,
+    per_page: int = 100,
+    output_csv: str = 'flickr_photos.csv',
+    start_date: Optional[str] = None,
+    end_date: Optional[str] = None
+    ):
+    min_taken_date = date_to_unix(start_date)
+    max_taken_date = date_to_unix(end_date)
     with open(output_csv, 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['id', 'title', 'url', 'tags', 'date_taken']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -17,13 +36,13 @@ def scrape_flickr(tag, max_pages=5, per_page=100, output_csv='flickr_photos.csv'
         for page in range(1, max_pages + 1):
             try:
                 print(f'Fetching page {page} for tag "{tag}"...')
-                photos = flickr.photos.search(tags=tag, per_page=per_page, page=page, extras='tags,date_taken,url_o,url_c,url_l,url_m')
+                photos = flickr.photos.search(tags=tag, per_page=per_page, page=page, 	extras='tags,date_taken,url_o,url_c,url_l,url_m', min_taken_date = min_taken_date, max_taken_date = max_taken_date)
                 photo_list = photos['photos']['photo']
                 if not photo_list:
                     print('No more photos found.')
                     break
                 for photo in photo_list:
-                    # Finding the most optimum URL.
+                    # Trying to find the most optimum url
                     url = photo.get('url_o') or photo.get('url_l') or photo.get('url_c') or photo.get('url_m') or ''
                     writer.writerow({
                         'id': photo['id'],
@@ -32,15 +51,14 @@ def scrape_flickr(tag, max_pages=5, per_page=100, output_csv='flickr_photos.csv'
                         'tags': photo.get('tags', ''),
                         'date_taken': photo.get('date_taken', '')
                     })
-                time.sleep(1)  # Για να μην ζορίσουμε το API πολύ
+                time.sleep(1)  # For not overchallenge the API
             except Exception as e:
                 print(f'Error fetching page {page}: {e}')
                 break
 
-#example, usage 1    
+#example
 if __name__ == "__main__":
-    # Example: Searching for photos, using the tag #nature.
-    scrape_flickr('nature', max_pages=3)
+    scrape_flickr('nature', max_pages=3, start_date='2020-09-10', end_date='2020-10-30')
     
-#example, usage 2
-scrape_flickr('nature', max_pages=3, per_page=3) #custom max_pages and per_page
+#example
+#scrape_flickr('nature', max_pages=3, per_page=3, start_date = '2020-09-10', end_date = '2020-10-30') #custom max_pages and per_page
